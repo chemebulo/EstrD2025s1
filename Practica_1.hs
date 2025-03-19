23eb3a05d90b82e514b0e3498839ef9e20c49d0b
Benjamin Maldonado <benjamindelpiero222@gmail.com>
2025-03-19 14:45:18 -0300

    Add Punto 4: Registros.

--- Practica_1.hs
+++ Practica_1.hs
@@ -1,27 +1,27 @@
 -- PUNTO 2: Números Enteros. 
 
--- EJERCICIO 1, A:
+-- EJERCICIO 2.1, A:
 
 sucesor :: Int -> Int
 -- PRECOND: Ninguna.
 sucesor x = x + 1
 
 
--- EJERCICIO 1, B:
+-- EJERCICIO 2.1, B:
 
 sumar :: Int -> Int -> Int
 -- PRECOND: Ninguna. 
 sumar x y = x + y
 
 
--- EJERCICIO 1, C:
+-- EJERCICIO 2.1, C:
 
 divisionYResto :: Int -> Int -> (Int, Int)
 -- PRECOND: El valor del parámetro "y" tiene que ser diferente de cero.
 divisionYResto x y = (div x y, mod x y)
 
 
--- EJERCICIO 1, D (Usando Alternativa condicional):
+-- EJERCICIO 2.1, D (Usando Alternativa condicional):
 
 maxDelPar :: (Int, Int) -> Int
 -- PRECOND: Ninguna.
@@ -45,7 +45,7 @@ data Dir = Norte | Sur | Este | Oeste
     deriving Show
 
 
--- EJERCICIO 1, A:
+-- EJERCICIO 3.1, A:
 
 opuesto :: Dir -> Dir
 -- PRECOND: Ninguna.
@@ -55,7 +55,7 @@ opuesto Sur   = Norte
 opuesto Oeste = Este
 
 
--- EJERCICIO 1, B:
+-- EJERCICIO 3.1, B:
 
 iguales :: Dir -> Dir -> Bool
 -- PRECOND: Ninguna.
@@ -67,7 +67,7 @@ iguales _ _         = False
 -- ¿Habrá otra forma de hacer este ejercicio?
 
 
--- EJERCICIO 1, C:
+-- EJERCICIO 3.1, C:
 
 siguiente :: Dir -> Dir
 -- PRECOND: El parámetro no puede usar Oeste como valor.
@@ -79,7 +79,7 @@ siguiente Sur   = Oeste
    quedar explícita tal restricción para no dar lugar a un error involuntario en la invocación. -}
 
 
--- EJERCICIO 2, A:
+-- EJERCICIO 3.2, A:
 
 data DiaDeSemana = Lunes | Martes | Miércoles | Jueves | Viernes | Sábado | Domingo
     deriving Show
@@ -97,7 +97,7 @@ ultimoDiaDeSemana :: DiaDeSemana
 ultimoDiaDeSemana = Domingo
 
 
--- EJERCICIO 2, B:
+-- EJERCICIO 3.2, B:
 
 empiezaConM :: DiaDeSemana -> Bool
 -- PRECOND: Ninguna.
@@ -106,7 +106,7 @@ empiezaConM Miércoles = True
 empiezaConM _         = False
 
 
--- EJERCICIO 2, C:
+-- EJERCICIO 3.2, C:
 
 vieneDespues :: DiaDeSemana -> DiaDeSemana -> Bool
 -- PRECOND: Ninguna.
@@ -123,7 +123,7 @@ numeroDeDia Sábado    = 6
 numeroDeDia Domingo   = 7
 
 
--- EJERCICIO 2, D:
+-- EJERCICIO 3.2, D:
 
 estaEnElMedio :: DiaDeSemana -> Bool
 -- PRECOND: Ninguna.
@@ -132,7 +132,7 @@ estaEnElMedio Domingo = False
 estaEnElMedio _       = True
 
 
--- EJERCICIO 3, A:
+-- EJERCICIO 3.3, A:
 
 negar :: Bool -> Bool
 -- PRECOND: Ninguna.
@@ -140,7 +140,7 @@ negar True = False
 negar False = True
 
 
--- EJERCICIO 3, B:
+-- EJERCICIO 3.3, B:
 
 implica :: Bool -> Bool -> Bool
 -- PRECOND: Ninguna.
@@ -152,7 +152,7 @@ esTrueYFalse True False = True
 esTrueYFalse _ _        = False
 
 
--- EJERCICIO 3, C:
+-- EJERCICIO 3.3, C:
 
 yTambien :: Bool -> Bool -> Bool
 -- PRECOND: Ninguna.
@@ -164,7 +164,7 @@ esTrueYTrue True True = True
 esTrueYTrue _ _       = False
 
 
--- EJERCICIO 3, D:
+-- EJERCICIO 3.3, D:
 
 oBien :: Bool -> Bool -> Bool
 -- PRECOND: Ninguna.
@@ -175,3 +175,92 @@ algunoEsTrue :: Bool -> Bool -> Bool
 algunoEsTrue True _ = True
 algunoEsTrue _ True = True
 algunoEsTrue _ _    = False
+
+
+-- PUNTO 4: Registros.
+
+data Persona = P String Int
+              -- Nombre Edad
+    deriving Show
+
+-- EJERCICIO 4.1:
+
+nombre :: Persona -> String
+-- PRECOND: El campo "n" de la persona a utilizar debe tener un nombre válido.
+nombre (P n e) = n
+
+
+edad :: Persona -> Int
+-- PRECOND: El campo "e" de la persona a utilizar debe ser mayor o igual a 0.
+edad (P n e) = e
+
+
+crecer :: Persona -> Persona
+-- PRECOND: El campo "e" de la persona a utilizar debe ser mayor o igual a 0.
+crecer (P n e) = P n (e + 1)
+
+
+cambioDeNombre :: String -> Persona -> Persona
+-- PRECOND: Ninguna.
+cambioDeNombre nom (P n e) = P nom e
+
+
+esMayorQueLaOtra :: Persona -> Persona -> Bool
+-- PRECOND: Ninguna.
+esMayorQueLaOtra (P nom1 edad1) (P nom2 edad2) = edad (P nom1 edad1) > edad (P nom2 edad2)
+
+
+laQueEsMayor :: Persona -> Persona -> Persona
+-- PERCOND: Ninguna.
+laQueEsMayor (P nom1 edad1) (P nom2 edad2) = if esMayorQueLaOtra (P nom1 edad1) (P nom2 edad2)
+                                             then P nom1 edad1
+                                             else P nom2 edad2
+
+
+-- EJERCICIO 4.2:
+
+data Pokemon = Poke TipoDePokemon Int
+                               -- Porcentaje de energía.
+    deriving Show
+
+
+data Entrenador = Entr String Pokemon Pokemon
+               -- Nombre Pokemon1 Pokemon2
+    deriving Show
+
+
+data TipoDePokemon = Agua | Fuego | Planta
+    deriving Show
+
+
+superaA :: Pokemon -> Pokemon -> Bool
+-- PRECOND: Ninguna.
+superaA (Poke tipo1 porcen1) (Poke tipo2 porcen2) = tipoDeEsSuperiorQue tipo1 tipo2
+
+tipoDeEsSuperiorQue :: TipoDePokemon -> TipoDePokemon -> Bool
+-- PRECOND: Ninguna.
+tipoDeEsSuperiorQue Agua Fuego   = True
+tipoDeEsSuperiorQue Fuego Planta = True
+tipoDeEsSuperiorQue Planta Agua  = True
+tipoDeEsSuperiorQue _ _          = False
+
+
+cantidadDePokemonDe :: TipoDePokemon -> Entrenador -> Int
+-- PRECOND: Ninguna.
+cantidadDePokemonDe tipo (Entr nomb poke1 poke2) = (unoSiPokemonEsTipoCeroSino poke1 tipo) + (unoSiPokemonEsTipoCeroSino poke2 tipo)
+
+unoSiPokemonEsTipoCeroSino :: Pokemon -> TipoDePokemon -> Int
+-- PRECOND: Ninguna.
+unoSiPokemonEsTipoCeroSino (Poke tipo1 porcen1) tipo2 = unoSiTipoEsMismoTipoQue tipo1 tipo2
+
+unoSiTipoEsMismoTipoQue :: TipoDePokemon -> TipoDePokemon -> Int
+-- PRECOND: Ninguna.
+unoSiTipoEsMismoTipoQue Agua Agua     = 1
+unoSiTipoEsMismoTipoQue Fuego Fuego   = 1
+unoSiTipoEsMismoTipoQue Planta Planta = 1
+unoSiTipoEsMismoTipoQue _ _           = 0
+
+
+juntarPokemon :: (Entrenador, Entrenador) -> [Pokemon]
+-- PRECOND: Ninguna.
+juntarPokemon ((Entr nom1 poke1 poke2), (Entr nom2 poke3 poke4)) = [poke1, poke2, poke3, poke4]
