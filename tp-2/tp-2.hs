678202f7ddd99368b2c938e3a19918c2833ea8c8
Benjamin Maldonado <benjamindelpiero222@gmail.com>
2025-03-24 20:55:15 -0300

    Add Ejercicios 1.1 al 1.9.

--- tp-2/tp-2.hs
+++ tp-2/tp-2.hs
@@ -1,6 +1,8 @@
 -- PUNTO 1: Recursión sobre listas.
 
 -- EJERCICIO 1.1:
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use foldr" #-}
 
 sumatoria :: [Int] -> Int
 -- PRECOND: Ninguna.
@@ -18,35 +20,62 @@ longitud (x:xs) = 1 + longitud xs
 
 -- EJERCICIO 1.3:
 
+sucesores :: [Int] -> [Int]
+-- PRECOND: Ninguna.
+sucesores []     = []
+sucesores (n:ns) = (n+1) : sucesores ns
 
 
 -- EJERCICIO 1.4:
 
+conjuncion :: [Bool] -> Bool
+-- PRECOND: Ninguna.
+conjuncion []     = True
+conjuncion (b:bs) = b && conjuncion bs
 
 
 -- EJERCICIO 1.5:
 
-
+disyuncion :: [Bool] -> Bool
+-- PRECOND: Ninguna.
+disyuncion []     = False
+disyuncion (b:bs) = b || disyuncion bs
 
 
 -- EJERCICIO 1.6:
 
-
-
+aplanar :: [[a]] -> [a]
+-- PRECOND: Ninguna.
+aplanar []     = []
+aplanar (x:xs) = x ++ aplanar xs 
 
 
 -- EJERCICIO 1.7:
 
-
+pertenece :: Eq a => a -> [a] -> Bool
+-- PRECOND: Ninguna.
+pertenece a []     = False 
+pertenece a (x:xs) = a == x || pertenece a xs
 
 
 -- EJERCICIO 1.8:
 
-
+apariciones :: Eq a => a -> [a] -> Int
+-- PRECOND: Ninguna.
+apariciones a []     = 0
+apariciones a (x:xs) = if a == x
+                       then 1 + apariciones a xs
+                       else apariciones a xs
 
 
 -- EJERCICIO 1.9:
 
+losMenoresA :: Int -> [Int] -> [Int]
+-- PRECOND: Ninguna.
+losMenoresA n []     = []
+losMenoresA n (ns:nss) = if ns < n
+                         then ns : losMenoresA n nss
+                         else losMenoresA n nss
 
 
 -- EJERCICIO 1.10:
