8d5a49148e08f1e304e493467744a4e8b20a6f82
Benjamin Maldonado <benjamindelpiero222@gmail.com>
2025-03-29 19:07:45 -0300

    Add avances varios en TP3.

--- tp-3/tp-3.hs
+++ tp-3/tp-3.hs
@@ -1,3 +1,19 @@
--- PUNTO 1: Tipos recursivos simles.
+-- PUNTO 1: Tipos recursivos simples.
 
--- EJERCICIO 1.1:
+-- EJERCICIO 1.1 (Celdas con bolitas):
+
+
+
+-- EJERCICIO 1.2 (Camino hacia el tesoro):
+
+
+
+
+
+-- PUNTO 2: Tipos arbóleos.
+
+-- EJERCICIO 2.1 (Árboles Binarios):
+
+
+
+-- EJERCICIO 2.2 (Expresiones Aritméticas):
