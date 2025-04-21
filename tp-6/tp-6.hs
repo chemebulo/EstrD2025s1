ad342fb42ebfa34d0f690031a90cf8f3124cf0a4
Benjamin Maldonado <benjamindelpiero222@gmail.com>
2025-04-21 00:03:36 -0300

    Add TP6.

---
+++ tp-6/tp-6.hs
@@ -0,0 +1,24 @@
+-- EJERCICIO 1: Priority Queue (Cola de Prioridad).
+
+
+-- EJERCICIO 1.1:
+
+-- EJERCICIO 1.2:
+
+
+
+-- EJERCICIO 2: Map (Diccionario).
+
+
+-- EJERCICIO 2.1:
+
+-- EJERCICIO 2.2:
+
+-- EJERCICIO 2.3:
+
+
+
+-- EJERCICIO 3: MultiSet (Multiconjunto).
+
+
+-- EJERCICIO 3.1:
