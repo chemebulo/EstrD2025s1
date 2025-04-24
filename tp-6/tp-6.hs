37b5ff0b03a0a3b9268bc6935967b876dc348630
Benjamin Maldonado <benjamindelpiero222@gmail.com>
2025-04-24 17:09:50 -0300

    Más avances en TP6.

--- tp-6/tp-6.hs
+++ tp-6/tp-6.hs
@@ -73,27 +73,48 @@ mapALista m (k:ks) = let v = fromJust (lookupM k m)
 
 fromJust :: Maybe a -> a
 fromJust Nothing   = error "No tendria que dar esto."
-fromJust (Just x)  = x 
+fromJust (Just x)  = x
 
 
+-- ARREGLARRRRRRRRRRRRRRRRRRRRR!!!!!!!!!!!!!!!!!
 agruparEq :: Eq k => [(k, v)] -> Map k [v]
 -- PROP: Dada una lista de pares clave valor, agrupa los valores de los pares que compartan la misma clave.
 -- O().
-agruparEq []       = undefined
-agruparEq (kv:kvs) = undefined
+agruparEq []          = emptyM
+agruparEq ((k,v):kvs) = agregarValueAK k v (agruparEq kvs)
+
+agregarValueAK :: Eq k => k -> v -> Map k [v] -> Map k [v]
+agregarValueAK k v m = listToMap (agregarVKEn k v (mapToList m))
+
+agregarVKEn :: Eq k => k -> v -> [(k,[v])] -> [(k,[v])]
+agregarVKEn nk nv []            = [(nk,[nv])]
+agregarVKEn nk nv ((k,vs):kvs) = if nk == k
+                                    then (k, nv:vs) : kvs
+                                    else (k,vs):kvs ++ agregarVKEn nk nv kvs
 
 
 incrementar :: Eq k => [k] -> Map k Int -> Map k Int
 -- PROP: Dada una lista de claves de tipo k y un map que va de k a Int, le suma uno a cada número asociado con dichas claves.
--- O().
-incrementar = undefined
+-- O(n^2).
+incrementar ks m = mapConIncrementoEnUnoEnKS m ks
+
+mapConIncrementoEnUnoEnKS :: Eq k => Map k Int -> [k] -> Map k Int
+mapConIncrementoEnUnoEnKS m []     = m
+mapConIncrementoEnUnoEnKS m (k:ks) = let v = fromJust (lookupM k m)
+                                     in if estaAsociadoEn k m
+                                           then assocM k (v + 1) (mapConIncrementoEnUnoEnKS m ks)
+                                           else mapConIncrementoEnUnoEnKS m ks
 
 
 mergeMaps:: Eq k => Map k v -> Map k v -> Map k v
 -- PROP: Dado dos maps se agregan las claves y valores del primer map en el segundo. Si una clave del primero  
 --       existe en el segundo, es reemplazada por la del primero.
 -- O().
-mergeMaps = undefined
+mergeMaps m1 m2 = agruparListaDeMapAMap (mapToList m1) m2
+
+agruparListaDeMapAMap :: Eq k => [(k, v)] -> Map k v -> Map k v
+agruparListaDeMapAMap []          m = m
+agruparListaDeMapAMap ((k,v):kvs) m = assocM k v (agruparListaDeMapAMap kvs m)
 
 
 -- EJERCICIO 2.2:
@@ -106,8 +127,12 @@ mergeMaps = undefined
 
 indexar :: [a] -> Map Int a
 -- PROP: Dada una lista de elementos construye un map que relaciona cada elemento con su posición en la lista.
--- O().
-indexar = undefined
+-- O(n).
+indexar xs = indexarXS 1 xs
+
+indexarXS :: Int -> [a] -> Map Int a
+indexarXS n []     = emptyM
+indexarXS n (x:xs) = assocM n x (indexarXS (n+1) xs)
 
 
 ocurrencias :: String -> Map Char Int
