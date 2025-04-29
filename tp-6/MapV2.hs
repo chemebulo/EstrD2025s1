b99e64b9b85641450225cbbc012c008008dea08a
Benjamin Maldonado <benjamindelpiero222@gmail.com>
2025-04-29 14:54:14 -0300

    Mejoras en los contratos de eficiencia.

--- tp-6/MapV2.hs
+++ tp-6/MapV2.hs
@@ -18,50 +18,86 @@ data Map k v = M [(k, v)]
 
 -}
 
------------------------------------------------- IMPLEMENTACIÓN ------------------------------------------------ 
+-- #################################################### IMPLEMENTACIÓN ####################################################
 
 emptyM :: Map k v
 -- PROP: Devuelve un map vacío.
+    -- COSTO: O(1).
+    -- Siendo en el peor caso de costo constante por el hecho de solamente utilizar el constructor con una lista vacía. 
 emptyM = M []
 
 
 assocM :: Eq k => k -> v -> Map k v -> Map k v
 -- PROP: Agrega una asociación clave-valor al map.
+    -- COSTO: O(1).
+    -- Siendo kvs la lista de pares clave-valor dada, k la clave dada y v el valor dado, se utiliza la operación "cons" en kvs
+    -- de costo constante en el peor caso. 
 assocM k v (M kvs) = M ((k,v):kvs)
 
 
 lookupM :: Eq k => k -> Map k v -> Maybe v
 -- PROP: Encuentra un valor dado una clave.
+    -- COSTO: O(n).
+    -- Siendo kvs la lista de pares clave-valor dada, se realiza una operación lineal sobre la misma. En el peor caso,
+    -- (kvs sin la clave o está al final) termina encontrando el valor de la clave dada al final de la lista, resultando 
+    -- en que "lookupM" resulta de costo lineal.
 lookupM k (M kvs) = encontrarEn k kvs
 
+
+deleteM :: Eq k => k -> Map k v -> Map k v
+-- PROP: Borra una asociación dada una clave.
+    -- COSTO: O(n).
+    -- Siendo kvs la lista de pares clave-valor dada, se realiza una operación lineal sobre la misma. En el peor caso,
+    -- aunque en realidad siempre pasa, llega al final de la lista borrando todas las claves que coincidan con "k".
+    -- Esto resulta en que "deleteM" termine siendo de costo lineal.
+deleteM k (M kvs) = M (eliminarEn k kvs)
+
+
+keys :: Eq k => Map k v -> [k]
+-- PROP: Devuelve las claves del map.
+    -- COSTO: O(n^2).
+    -- Siendo kvs la lista de pares clave-valor dada, se realiza una operacion de costo 'n^2' sobre la misma. En el 
+    -- peor caso (aunque asegurado que termina haciendo eso), agrega todas las claves de kvs en una lista, resultando
+    -- que "keys" termine siendo de costo 'n^2'.
+keys (M kvs) = soloKeys kvs
+
+-- #################################################### AUXILIARES ####################################################
+
 encontrarEn :: Eq k => k -> [(k, v)] -> Maybe v
+    -- COSTO: O(n).
+    -- Siendo kvs la lista de pares clave-valor dada, se realiza una operación constante entre la clave dada "nk" y cada
+    -- clave de kvs, resultando de costo lineal ya que el peor caso posible es que termine llegando al final de kvs, ya
+    -- sea porque no termine encontrando el valor de la clave dada, o porque está justo al final de kvs.
 encontrarEn nk []          = Nothing
 encontrarEn nk ((k,v):kvs) = if nk == k
                                 then Just v
                                 else encontrarEn nk kvs
 
 
-deleteM :: Eq k => k -> Map k v -> Map k v
--- PROP: Borra una asociación dada una clave.
-deleteM k (M kvs) = M (eliminarEn k kvs)
-
 eliminarEn :: Eq k => k -> [(k,v)] -> [(k,v)]
+    -- COSTO: O(n).
+    -- Siendo kvs la lista de pares clave-valor dada, se realiza una operación constante entre la clave dada "nk" y cada
+    -- clave de kvs, y encima por como está construida, tiene asegurado llegar al final de la lista ya que elimina cada
+    -- uno de las claves en kvs que coincidan con "nk". Resultando que "eliminarEn" sea de costo lineal.
 eliminarEn nk []          = []
 eliminarEn nk ((k,v):kvs) = if nk == k
                                then eliminarEn nk kvs
                                else (k,v) : eliminarEn nk kvs
 
 
-keys :: Eq k => Map k v -> [k]
--- PROP: Devuelve las claves del map.
-keys (M kvs) = soloKeys kvs
-
 soloKeys :: Eq k => [(k, v)] -> [k]
+    -- COSTO: O(n^2).
+    -- Siendo n la cantidad de elementos de la lista de pares clave-valor dada, se realiza una operación de costo lineal n
+    -- para cada n. En el peor caso (aunque asegurado que va a hacer eso), resulta que el costo de "soloKeys" sea de 'n^2'.
 soloKeys []          = []
 soloKeys ((k,v):kvs) = if not (estaEnPares k kvs)
                           then k : soloKeys kvs
                           else soloKeys kvs
 
 estaEnPares :: Eq k => k -> [(k, v)] -> Bool
+    -- COSTO: O(n).
+    -- Siendo kvs la lista de pares clave-valor dada, se realiza una operación constante para cada clave de kvs. En el
+    -- peor caso termina llegando al final de kvs preguntando si "nk" es la clave del par de ese momento. Esto resulta
+    -- en que "estaEnPares" sea de costo lineal.
 estaEnPares nk []          = False
 estaEnPares nk ((k,v):kvs) = nk == k || estaEnPares nk kvs
--- tp-6/MapV3.hs
+++ tp-6/MapV3.hs
@@ -19,54 +19,85 @@ data Map k v = M [k] [v]
 
 -}
 
------------------------------------------------- IMPLEMENTACIÓN ------------------------------------------------ 
+-- #################################################### IMPLEMENTACIÓN #################################################### 
 
 emptyM :: Map k v
--- PROP: Devuelve un map vacío
+-- PROP: Devuelve un map vacío.
+    -- COSTO: O(1).
+    -- Siendo en el peor caso de costo constante por el hecho de solamente utilizar el constructor con dos listas vacías.
 emptyM = M [] []
 
 
 assocM :: Eq k => k -> v -> Map k v -> Map k v
 -- PROP: Agrega una asociación clave-valor al map.
+    -- COSTO: O(n).
+    -- Siendo ks1 la lista de claves dada, y vs1 la lista de valores dado, se realiza una operación de costo lineal para
+    -- ks1 y vs1, utilizando además k y v (clave y valor nuevo dado). Siempre recorre hasta el final por diseño, incluso 
+    -- si no encuentra la clave, y después la agrega igual, lo cual termina resultando en que "assocM" sea de costo lineal.
 assocM k v (M ks1 vs1) = let (ks, vs) = agregarEn k v ks1 vs1
                          in M ks vs
-
-agregarEn :: Eq k => k -> v -> [k] -> [v] -> ([k],[v])
--- PRECOND: Las listas dadas deben tener la misma longitud.
-agregarEn nk nv []     _      = ([nk], [nv])
-agregarEn nk nv (k:ks) (v:vs) = if nk == k
-                                   then (nk:ks, nv:vs)
-                                   else agregar nk nv (agregarEn nk nv ks vs)
-
-agregar :: k -> v -> ([k], [v]) -> ([k], [v])
-agregar nk nv (ks, vs) = (nk:ks, nv:vs)
 
 
 lookupM :: Eq k => k -> Map k v -> Maybe v
 -- PROP: Encuentra un valor dado una clave.
+    -- COSTO: O(n).
+    -- Siendo ks la lista de claves dada, y vs la lista de valores dado, se realiza una operación de costo lineal para
+    -- ks y vs, utilizando además k (clave dada), lo cual termina resultando en que "lookupM" sea de costo lineal.
 lookupM k (M ks vs) = valueEn k ks vs
+
+
+deleteM :: Eq k => k -> Map k v -> Map k v
+-- PROP: Borra una asociación dada una clave.
+    -- COSTO: O(n).
+    -- Siendo ks la lista de claves dada, y vs la lista de valores dado, se realiza una operacion de costo lineal para
+    -- ks y vs, utilizando además k (clave dada), lo cual termina resultando en que "deleteM" sea de costo lineal. 
+deleteM k (M ks ys) = let (ks, vs) = eliminarM k ks vs
+                      in M ks vs
+
+
+keys :: Map k v -> [k]
+-- PROP: Devuelve las claves del map.
+    -- COSTO: O(1).
+    -- Siendo ks la lista de claves dada, y vs la lista de valores dado, solamente se devuelve ks, lo cual es de costo
+    -- constante.
+keys (M ks vs) = ks
+
+-- #################################################### AUXILIARES ####################################################
+
+agregarEn :: Eq k => k -> v -> [k] -> [v] -> ([k],[v])
+-- PRECOND: Las listas dadas deben tener la misma longitud.
+    -- COSTO: O(n).
+    -- Siendo ks la lista de claves dada, y vs la lista de valores dado, se realiza una operación de comparación de costo
+    -- constante para cada clave de ks. En el peor caso, la clave a la que se le quiere agregar un nuevo valor está al final 
+    -- de la lista de ks o se termina llegando al final de la lista y no esta la clave dada. Esto resulta en que "agregarEn"
+    -- es de costo lineal.
+agregarEn nk nv []     _      = ([nk], [nv])
+agregarEn nk nv (k:ks) (v:vs) = if nk == k
+                                   then (k:ks, nv:vs)
+                                   else let (ks', vs') = agregarEn nk nv ks vs
+                                        in  (k:ks', v:vs')
+
 
 valueEn :: Eq k => k -> [k] -> [v] -> Maybe v
 -- PRECOND: Las listas dadas deben tener la misma longitud.
+    -- COSTO: O(n).
+    -- Siendo ks la lista de claves dada, y vs la lista de valores dado, se realiza una operación de comparación de costo
+    -- constante para cada clave de ks. El peor caso es que el valor de la clave a buscar está al final de la lista o que
+    -- en realidad llegue al final de la lista y no esté. Es por eso que "valueEn" termina resultando ser de costo lineal.
 valueEn nk []     _      = Nothing
 valueEn nk (k:ks) (v:vs) = if nk == k
                               then Just v
                               else valueEn nk ks vs  
 
 
-deleteM :: Eq k => k -> Map k v -> Map k v
--- PROP: Borra una asociación dada una clave.
-deleteM k (M xs ys) = let (ks, vs) = eliminarM k ks vs
-                      in M ks vs
-
 eliminarM :: Eq k => k -> [k] -> [v] -> ([k],[v])
 -- PRECOND: Las listas dadas deben tener la misma longitud.
+    -- COSTO: O(n).
+    -- Siendo ks la lista de claves dada, y vs la lista de valores dado, se realiza una operación de comparación de costo
+    -- constante para cada clave de ks. El peor caso es que la clave a buscar está al final de la lista o que en realidad
+    -- llegue al final de la lista y no esté. Es por eso que "eliminarM" resulta ser de costo lineal.
 eliminarM nk [] _          = ([], [])
 eliminarM nk (k:ks) (v:vs) = if nk == k
                                 then (ks, vs)
-                                else agregar k v (eliminarM nk ks vs)
-
-
-keys :: Map k v -> [k]
--- PROP: Devuelve las claves del map.
-keys (M ks vs) = ks
+                                else let (ks', vs') = eliminarM nk ks vs
+                                     in  (k:ks', v:vs')
