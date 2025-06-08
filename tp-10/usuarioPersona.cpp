1a71cc5ff3f087864593cb5dd318f1893b5b17bf
Benjamin Maldonado <benjamindelpiero222@gmail.com>
2025-06-08 19:33:37 -0300

    Add: 1.1 TAD Persona terminado.

---
+++ tp-10/usuarioPersona.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "Persona.h"
+using namespace std;
+
+/* INTERFAZ DE PERSONA:
+     * Persona consPersona(string nombre, int edad);
+     * string nombre(Persona p);
+     * int edad(Persona p);
+     * void crecer(Persona p);
+     * void cambioDeNombre(string nombre, Persona p);
+     * bool esMayorQueLaOtra(Persona p1, Persona p2);
+     * Persona laQueEsMayor(Persona p1, Persona p2);
+*/
+
+int main() {
+    Persona juan = consPersona("Juan", 40);
+    Persona ricardo = consPersona("Ricardo", 27);
+    Persona mai = consPersona("Mai", 33);
+    cout << "El nombre de la primera persona es " << nombre(juan) <<    " y tiene " << edad(juan) << " anios." << endl;
+    cout << "El nombre de la segunda persona es " << nombre(ricardo) << " y tiene " << edad(ricardo) << " anios." << endl;
+    cout << "El nombre de la tercera persona es " << nombre(mai) <<     " y tiene " << edad(mai) << " anios." << endl;
+    crecer(juan);
+    crecer(mai);
+    cout << "Paso un anio, y actualmente Juan tiene " << edad(juan) << " anios." << endl;
+    cout << "Paso un anio, y actualmente Mai tiene " << edad(mai) << " anios." << endl;
+    cambioDeNombre("Juan Carlos", juan);
+    Persona mayor = laQueEsMayor(juan, ricardo);
+    cout << "Juan se cambio el nombre a " << nombre(juan) << "." << endl;
+    cout << boolalpha << "Es verdad que Mai es mas grande que Juan:    " << esMayorQueLaOtra(mai, juan) << endl;
+    cout << boolalpha << "Es verdad que Ricardo es mas grande que Mai: " << esMayorQueLaOtra(ricardo, mai) << endl;
+    cout << "La persona mas grande entre Juan Carlos y Ricardo es " << nombre(mayor) << " con "<< edad(mayor) << " anios." << endl; 
+    return 0;
+}
