d2402d168c974c96b998eedc2bc2c6e7d5e3570f
Benjamin Maldonado <benjamindelpiero222@gmail.com>
2025-06-19 17:40:35 -0300

    Add: UFSet primeros avances.

---
+++ repaso-segundo-parcial/UFSet.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "UFSet.h"
+
+UFSet createUFS(ELEM_TYPE value){
+// PROPÓSITO: Inicializa el UFSet ufset, cuyo valor asociado será value.
+
+}
+
+UFSet findUFS(UFSet elem){
+// PROPÓSITO: Encuentra el elemento distinguido para el UFSet dado.
+
+}
+
+void unionUFS(UFSet ufset1, UFSet ufset2){
+// PROPÓSITO: Calcula la unión entre los conjuntos ufset1 y ufset2.
+
+}
+
+ELEM_TYPE elemUFS(UFSet ufset){
+// PROPÓSITO: Devuelve el valor asociado a elemento de tipo UFSet.
+
+}
