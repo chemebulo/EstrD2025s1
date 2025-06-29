0af69331a20c4a2cd294053dd567945205f655b3
Benjamin Maldonado <benjamindelpiero222@gmail.com>
2025-06-28 21:46:57 -0300

    Add: Primera optimización completada.

--- repaso-segundo-parcial/UFSet.cpp
+++ repaso-segundo-parcial/UFSet.cpp
@@ -4,15 +4,17 @@
 struct UFNode{
     ELEM_TYPE valor;
     UFNode* padre;
+    int rango;
 };
 
 UFSet createUFS(ELEM_TYPE value){
 // PROPÓSITO: Inicializa el UFSet ufset, cuyo valor asociado será value.
-// COSTO TIEMPO: O().
-// COSTO ESPACIAL: O().
+// COSTO TIEMPO: O(1).
+// COSTO ESPACIAL: O(1).
     UFNode* ufset = new UFNode;
     ufset->valor = value;
     ufset->padre = ufset;
+    ufset->rango = 0;
     return ufset;
 }
 
@@ -28,19 +30,26 @@ UFSet findUFS(UFSet elem){
 
 void unionUFS(UFSet ufset1, UFSet ufset2){
 // PROPÓSITO: Calcula la unión entre los conjuntos ufset1 y ufset2.
-// COSTO TIEMPO: O().
-// COSTO ESPACIAL: O().
+// COSTO TIEMPO: O(n).
+// COSTO ESPACIAL: O(n).
     UFSet root1 = findUFS(ufset1);
     UFSet root2 = findUFS(ufset2);
 
     if(root1 != root2){
-        root2->padre = root1;
+        if(root1->rango < root2->rango){
+            root1->padre = root2;
+        } else if(root1->rango > root2->rango){
+            root2->padre = root1;
+        } else {
+            root2->padre = root1;
+            root1->rango += 1;
+        }
     }
 }
 
 ELEM_TYPE elemUFS(UFSet ufset){
 // PROPÓSITO: Devuelve el valor asociado a elemento de tipo UFSet.
-// COSTO TIEMPO: O().
-// COSTO ESPACIAL: O().
+// COSTO TIEMPO: O(1).
+// COSTO ESPACIAL: O(1).
     return ufset->valor;
 }
